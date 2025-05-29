49df4917ee6ac54f688632903665cca228ef57dd
vj999431@gmail.com <Vj999431@gmail.com>
2025-05-29 16:18:02 +0530

    g

--- app/page.jsx
+++ app/page.jsx
@@ -2,6 +2,15 @@
 import Brand from "./sections/brand";
 import Hero from "./sections/hero";
 
+import ThinkersClubChatbot from "@/components/chatbot";
+
+import Process from "./sections/process";
+import Thinkers from "./sections/thinkers";
+import Investertable from "./sections/investertable" ;
+
+
+
+
 export default function Page() {
   return (
     <div>
